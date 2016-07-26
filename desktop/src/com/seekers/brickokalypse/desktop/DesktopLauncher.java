363aa0d4f19e415d5067224a0fc9987430820ea6
millennia <aparshasanjana@yahoo.ca>
2016-07-25 21:41:33 -0400

    Brickokalypse game
    Android game / Desktop

---
+++ desktop/src/com/seekers/brickokalypse/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.seekers.brickokalypse.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.seekers.brickokalypse.Brickokalypse;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Brickokalypse(), config);
+	}
+}
---
+++ html/src/com/seekers/brickokalypse/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.seekers.brickokalypse.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.seekers.brickokalypse.Brickokalypse;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Brickokalypse();
+        }
+}
